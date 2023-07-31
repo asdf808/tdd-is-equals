fb833cd72f0be07daf5a762189f7b5e10d4c85b1
Roy.Liu <Roy.Liu@fanruan.com>
2023-08-01 01:52:37 +0800

    isEquals实现

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isEquals(a: any, b: any): boolean {
-  return a === b;
+  if(typeof a !== typeof b) {
+    return false;
+  }
+  if (typeof a === 'object') {
+    for(const key in a) {
+      if (!Object.hasOwn(b, key) || !isEquals(a[key], b[key])) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return a === b;
+  }
 }
